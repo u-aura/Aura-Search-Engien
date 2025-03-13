87ba7554b01c54be9f0780fdf5f8cc9b2740d73c
u-aura <malviyaaradhya99@gmail.com>
2025-03-13 14:57:32 +0530

    Create Aura .js
    Aura.js

---
+++ Aura .js
@@ -0,0 +1,9 @@
+// Aura.js
+// This file links search functionality with the frontend
+
+const search = require('./websitelink');
+
+document.getElementById("searchButton").addEventListener("click", function() {
+    const query = document.getElementById("searchInput").value;
+    search(query);
+});
